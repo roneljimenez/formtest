9b48b542c3c2c3c7e3f0f84049952d78cccf0ef5
Ronel <ronel_jimenez@hotmail.com>
2019-12-08 22:28:42 -0300

    version 1.0 operativa

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,11 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+        @if (isset($message))
+            <h5>{{$message}}</h5>
+        @endif
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Formulario</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,8 +16,19 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    You are logged in!
+                    @if ($usuario['canSendForm'])
+                        <form action="{{ route('sendForm') }}" method="POST">
+                            <div class="form-group">
+                                <label for="texto">Texto del envio</label>
+                                <textarea class="form-control" id="texto" rows="3" name="contenido"></textarea>
+                            </div>
+                            <input type="hidden" name="usuario" value="{{ $usuario['email'] }}">
+                            <button type="submit" class="btn btn-primary">Enviar</button>
+                            @csrf
+                        </form>
+                    @else
+                        <h4>Bienvend@ a la App, no tienes autorización para enviar Formularios</h4>
+                    @endif
                 </div>
             </div>
         </div>
